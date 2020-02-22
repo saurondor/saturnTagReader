13659178df63903e5817377d9f140e3cabc57ace
Gerardo Tasistro <gtasistro@tiempometa.com>
2020-02-22 17:12:35 -0600

    Change app title

--- src/main/java/com/tiempometa/pandora/ipicoreader/JSplashScreen.java
+++ src/main/java/com/tiempometa/pandora/ipicoreader/JSplashScreen.java
@@ -41,52 +41,50 @@ public class JSplashScreen extends JFrame {
 		progressBar = new JProgressBar();
 		statusLabel = new JLabel();
 
-		// ======== this ========
+		//======== this ========
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		setResizable(false);
 		setUndecorated(true);
-		setIconImage(new ImageIcon(
-				getClass().getResource("/com/tiempometa/pandora/ipicoreader/tiempometa_icon_large_alpha.png"))
-						.getImage());
+		setIconImage(new ImageIcon(getClass().getResource("/com/tiempometa/pandora/ipicoreader/tiempometa_icon_large_alpha.png")).getImage());
 		Container contentPane = getContentPane();
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
 
-		// ======== panel1 ========
+		//======== panel1 ========
 		{
 			panel1.setFont(new Font("Tahoma", Font.BOLD, 14));
-			panel1.setLayout(new FormLayout("20dlu, $lcgap, 179dlu, $lcgap, 159dlu",
-					"20dlu, 2*($lgap, default), $lgap, 26dlu, $lgap, default, $lgap, 86dlu, 2*($lgap, default)"));
+			panel1.setLayout(new FormLayout(
+				"20dlu, $lcgap, 179dlu, $lcgap, 159dlu",
+				"20dlu, 2*($lgap, default), $lgap, 26dlu, $lgap, default, $lgap, 86dlu, 2*($lgap, default)"));
 
-			// ---- button1 ----
-			button1.setIcon(new ImageIcon(
-					getClass().getResource("/com/tiempometa/pandora/ipicoreader/tiempometa_icon_large_alpha.png")));
+			//---- button1 ----
+			button1.setIcon(new ImageIcon(getClass().getResource("/com/tiempometa/pandora/ipicoreader/tiempometa_icon_large_alpha.png")));
 			button1.setFocusable(false);
 			panel1.add(button1, CC.xywh(3, 3, 1, 9));
 
-			// ---- label1 ----
+			//---- label1 ----
 			label1.setText(bundle.getString("JSplashScreen.label1.text"));
 			label1.setFont(new Font("Tahoma", Font.BOLD, 14));
 			label1.setHorizontalAlignment(SwingConstants.CENTER);
 			panel1.add(label1, CC.xy(5, 3));
 
-			// ---- label4 ----
+			//---- label4 ----
 			label4.setText(bundle.getString("JSplashScreen.label4.text"));
 			label4.setHorizontalAlignment(SwingConstants.CENTER);
 			panel1.add(label4, CC.xy(5, 5));
 
-			// ---- label3 ----
+			//---- label3 ----
 			label3.setText(bundle.getString("JSplashScreen.label3.text"));
 			label3.setHorizontalAlignment(SwingConstants.CENTER);
 			label3.setFont(new Font("Tahoma", Font.PLAIN, 12));
 			panel1.add(label3, CC.xy(5, 7));
 
-			// ---- label2 ----
+			//---- label2 ----
 			label2.setText(bundle.getString("JSplashScreen.label2.text"));
 			label2.setHorizontalAlignment(SwingConstants.CENTER);
 			panel1.add(label2, CC.xy(5, 9));
 			panel1.add(progressBar, CC.xywh(3, 13, 3, 1));
 
-			// ---- statusLabel ----
+			//---- statusLabel ----
 			statusLabel.setHorizontalAlignment(SwingConstants.CENTER);
 			panel1.add(statusLabel, CC.xywh(3, 15, 3, 1));
 		}
